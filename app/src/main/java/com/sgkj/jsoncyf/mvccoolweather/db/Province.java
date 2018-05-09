815c37427c329e01d88b733a5e32428d8f8b7ce0
JsonCyf <1026352702@qq.com>
2018-05-09 11:09:22 +0800

    cyf 加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/sgkj/jsoncyf/mvccoolweather/db/Province.java
@@ -0,0 +1,42 @@
+package com.sgkj.jsoncyf.mvccoolweather.db;
+
+/**
+ * Created by JsonCyf on 2018/5/9.
+ */
+
+public class Province {
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+    public Province() {
+    }
+    public Province(int id, String provinceName, int provinceCode) {
+        this.id = id;
+        this.provinceName = provinceName;
+        this.provinceCode = provinceCode;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName == null ? "" : provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
